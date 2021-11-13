0331e278938a93e2b6de713bcf4343c46c1d8ec1
João Costa <costav1402@gmail.com>
2021-11-13 17:51:51 -0300

    create write page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Home from './pages/home/Home';
 import Topbar from './components/topbar/Topbar';
 import Single from './pages/single/Single';
+import Write from './pages/write/Write';
 
 function App() {
   return (
     <>
     <Topbar />
-    {/*<Home/>*/}
-    <Single/>
+    {/*<Home/>
+    <Single/>*/}
+    <Write/>
     </>
   );
 }
