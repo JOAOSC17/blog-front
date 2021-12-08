f4e6b20854a9b8c3239c428e1e42bbf682e4ac2c
João Costa <costav1402@gmail.com>
2021-12-08 11:25:05 -0300

    create login system

---
+++ src/context/Action.js
@@ -0,0 +1,10 @@
+export const LoginStart = (userCredentials) => ({
+    type:"LOGIN_START",
+});
+export const LoginSucess = (user) => ({
+    type:"LOGIN_SUCESS",
+    payload:user,
+});
+export const LoginFailure = () => ({
+    type:"LOGIN_FAILURE",
+});
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,16 +1,36 @@
+import { useRef, useContext } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import "./Login.css";
-
+import { Context } from '../../context/Context'
 export default function Login() {
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { user, dispatch, isFetching } = useContext(Context)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({type:"LOGIN_START"})
+    try {
+      const res = await axios.post("http://localhost:5000/api/auth/login", {
+        username:userRef.current.value,
+        password:passwordRef.current.value
+      });
+      dispatch({type:"LOGIN_SUCESS", payload:res.data})
+    } catch (err) {
+      dispatch({type:"LOGIN_FAILURE"})
+    }
+  }
+  console.log(user)
+  console.log(isFetching)
   return (
     <div className="login">
       <span className="loginTitle">Login</span>
-      <form className="loginForm">
-        <label>Email</label>
-        <input className="loginInput" type="text" placeholder="Enter your email..." />
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input className="loginInput" ref={userRef} type="text" placeholder="Enter your username..." />
         <label>Password</label>
-        <input className="loginInput" type="password" placeholder="Enter your password..." />
-        <button className="loginButton">Login</button>
+        <input className="loginInput" ref={passwordRef} type="password" placeholder="Enter your password..." />
+        <button className="loginButton" type="submit">Login</button>
       </form>
         <Link to="/register" className="loginRegisterButton">Register</Link>
     </div>
